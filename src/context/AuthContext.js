bc072255eda9fa70ac71b5165ba084f68f5933f6
NicolasHDAraujo <nicolas_hd.araujo@outlook.com>
2022-02-22 18:35:56 -0300

    atualizações

--- src/context/AuthContext.js
+++ src/context/AuthContext.js
@@ -7,24 +7,28 @@ const AuthContext = createContext({})
 function AuthProvider({ children }) {
   const [authenticated, setAuthenticated] = useState(false);
 
-  async function signIn({email, password}) {
-    const data = {
-      email,
-      password
+  async function signIn({ email, password }) {
+    try {
+      const response = await api.post('/auth/login',
+        {
+          email, 
+          password
+        }
+      )
+
+      console.debug(response.data);
+
+      setAuthenticated(true);
+    } catch (err) {
+      console.error(err.message);
     }
-
-    const {data: { authorization }} = await api.post('/auth/login', data);
-
-    console.debug(authorization)
-
-    setAuthenticated(true);
   }
 
-  return(
-    <AuthContext.Provider value={{signIn, authenticated}}>
+  return (
+    <AuthContext.Provider value={{ signIn, authenticated }}>
       {children}
     </AuthContext.Provider>
   )
 }
 
-export { AuthContext , AuthProvider}
+export { AuthContext, AuthProvider }
